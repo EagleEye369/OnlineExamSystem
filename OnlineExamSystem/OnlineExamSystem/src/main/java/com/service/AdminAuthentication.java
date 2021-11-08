29c3d9d195afca130ccb2e05d1c096b3bcc8d797
EagleEye369 <93862970+EagleEye369@users.noreply.github.com>
2021-11-08 23:29:38 +0530

    Delete OnlineExamSystem/OnlineExamSystem directory

--- OnlineExamSystem/OnlineExamSystem/src/main/java/com/service/AdminAuthentication.java
+++
@@ -1,8 +0,0 @@
-package com.service;
-
-import com.advices.DataNotFoundedException;
-import com.entity.Admin;
-
-public interface AdminAuthentication {
-	public String adminLogin(Admin admin) throws DataNotFoundedException;
-}
